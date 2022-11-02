755abce5c95b8782c7813a815e85e74e1a1ccc38
iwatani-kenji <kleversonkenji@gmail.com>
2022-11-02 12:57:42 -0300

    finishes button component on react native

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,4 +1,6 @@
-import { Center, Text } from "native-base";
+import { Center, Icon } from "native-base";
+import { Button } from "../components/Button";
+import { Fontisto } from "@expo/vector-icons";
 
 import Logo from "../assets/logo.svg";
 
@@ -6,6 +8,11 @@ export function SignIn() {
   return (
     <Center flex={1} bgColor="gray.900">
       <Logo width={212} height={40} />
+      <Button
+        title="ENTRAR COM GOOGLE"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        type="SECONDARY"
+      />
     </Center>
   );
 }
