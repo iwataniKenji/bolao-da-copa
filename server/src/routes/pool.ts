44e3074a265102b5c226918b5e72a87c3f49dc1a
iwatani-kenji <kleversonkenji@gmail.com>
2022-11-04 20:03:38 -0300

    adds two routes from pool entity

--- server/src/routes/pool.ts
+++ server/src/routes/pool.ts
@@ -2,8 +2,10 @@ import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
 import ShortUniqueId from "short-unique-id";
+import { authenticate } from "../plugins/authenticate";
 
 export async function poolRoutes(fastify: FastifyInstance) {
+  // conta a quantidade de bolões
   fastify.get("/pools/count", async () => {
     // prisma tem autocomplete das tabelas criadas
     const count = await prisma.pool.count();
@@ -11,6 +13,7 @@ export async function poolRoutes(fastify: FastifyInstance) {
     return { count };
   });
 
+  // cria um bolão
   fastify.post("/pools", async (request, reply) => {
     // validação
     const createPoolBody = z.object({
@@ -22,13 +25,91 @@ export async function poolRoutes(fastify: FastifyInstance) {
     const generate = new ShortUniqueId({ length: 6 });
     const code = String(generate()).toUpperCase();
 
-    await prisma.pool.create({
-      data: {
-        title,
-        code,
-      },
-    });
+    try {
+      // se tem token, cria uma id com dono
+      await request.jwtVerify();
+
+      await prisma.pool.create({
+        data: {
+          title,
+          code,
+          ownerId: request.user.sub,
+
+          participants: {
+            create: {
+              userId: request.user.sub,
+            },
+          },
+        },
+      });
+    } catch {
+      // se não tiver token, cria uma pool sem dono
+      await prisma.pool.create({
+        data: {
+          title,
+          code,
+        },
+      });
+    }
 
     return reply.status(201).send({ code });
   });
+
+  // cria relação de participante do bolão
+  fastify.post(
+    "/pools/:id/join",
+    { onRequest: [authenticate] },
+    async (request, reply) => {
+      const joinPoolBody = z.object({
+        code: z.string(),
+      });
+
+      const { code } = joinPoolBody.parse(request.body);
+
+      const pool = await prisma.pool.findUnique({
+        where: {
+          code,
+        },
+        include: {
+          participants: {
+            where: {
+              userId: request.user.sub, // sub -> id do usuário logado
+            },
+          },
+        },
+      });
+
+      if (!pool) {
+        return reply.status(400).send({ message: "Pool not found" });
+      }
+
+      if (pool.participants.length > 0) {
+        return reply
+          .status(400)
+          .send({ message: "You already joined this pool" });
+      }
+
+      // se bolão não tiver dono, adiciona o primeiro participante como dono
+      if (!pool.ownerId) {
+        await prisma.pool.update({
+          where: {
+            id: pool.id,
+          },
+          data: {
+            ownerId: request.user.sub,
+          },
+        });
+      }
+
+      // cria relação entre usuário e pool (participante)
+      await prisma.participant.create({
+        data: {
+          poolId: pool.id,
+          userId: request.user.sub,
+        },
+      });
+
+      return reply.status(201).send();
+    }
+  );
 }
