f925feab096f33b79cb1124522f9a0b8932d9cac
iwatani-kenji <kleversonkenji@gmail.com>
2022-11-05 10:14:07 -0300

    uses authentication state to change route authorizations

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,12 +1,18 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { useAuth } from "../hooks/useAuth";
 import { AppRoutes } from "./app.routes";
 import { SignIn } from "../screens/SignIn";
+import { Box } from "native-base";
 
 // separa decisão sobre rotas caso usuário esteja logado ou não
 export function Routes() {
+  const { user } = useAuth();
+
   return (
-    <NavigationContainer>
-      <SignIn />
-    </NavigationContainer>
+    <Box flex={1} bg="gray.900">
+      <NavigationContainer>
+        {user.name ? <AppRoutes /> : <SignIn />}
+      </NavigationContainer>
+    </Box>
   );
 }
